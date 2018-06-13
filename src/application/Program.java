095fc64a6e83f7854f9c0d3ac932d3e90b8ce78f
Nelio Alves <acenelio@gmail.com>
2018-06-13 12:01:59 -0300

    Anonymous class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -15,7 +16,14 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		list.sort(new MyComparator());
+		Comparator<Product> comp = new Comparator<Product>() {
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};
+		
+		list.sort(comp);
 
 		for (Product p : list) {
 			System.out.println(p);
