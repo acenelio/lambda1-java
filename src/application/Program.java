433d649b5f641f28edeaed5c047cb24ed7e0cc2a
Nelio Alves <acenelio@gmail.com>
2018-06-13 12:04:21 -0300

    Lambda 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,9 +16,7 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 
-		Comparator<Product> comp = (p1, p2) -> {
-			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		};
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 
 		list.sort(comp);
 
