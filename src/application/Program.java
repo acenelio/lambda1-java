4aea3b8feba35f86987b7a6c8d200242c0859db3
Nelio Alves <acenelio@gmail.com>
2018-06-13 12:03:45 -0300

    Lambda 1

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,18 +11,15 @@ public class Program {
 	public static void main(String[] args) {
 
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
-		
-		Comparator<Product> comp = new Comparator<Product>() {
-			@Override
-			public int compare(Product p1, Product p2) {
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
+
+		Comparator<Product> comp = (p1, p2) -> {
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		};
-		
+
 		list.sort(comp);
 
 		for (Product p : list) {
